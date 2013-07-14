d383bb3fdcae95804a7ca76c5cd16e44009dd8ce
JShelto <jshelto@sas.upenn.edu>
2013-07-14 14:37:03 -0700

    Initial commit

---
+++ crime_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrimeApp::Application.config.secret_token = 'd289cc1597fc98951f59bf87e9febcd9da8c300ff144967aed6f5940d5be942ad44215dc9cec96a8c8813941aad0e3310c88fd5a8fdb254e3f0d60046f76e9ec'
