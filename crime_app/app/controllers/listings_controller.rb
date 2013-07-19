8f9d64fd93ddc8dddf54075d14ccc899225dbd72
JShelto <jshelto@sas.upenn.edu>
2013-07-18 19:51:34 -0700

    Circular divs added

--- crime_app/app/controllers/listings_controller.rb
+++ crime_app/app/controllers/listings_controller.rb
@@ -2,11 +2,11 @@ class ListingsController < ApplicationController
 
 	def index
 		@listings =[]
-		url = open("http://sfbay.craigslist.org/sfc/apa/")
-		# url = open("craigslist.html")
+		# url = open("http://sfbay.craigslist.org/sfc/apa/")
+		url = open("craigslist.html")
 		url_data = Nokogiri::HTML(url)
 		url_data.css(".row").each do |x|
-			x.css("a").each {|a| a["href"] = a["href"].insert(0,"http://sfbay.craigslist.org")}
+			# x.css("a").each {|a| a["href"] = a["href"].insert(0,"http://sfbay.craigslist.org")}
 			l = {}
 			l[:link] = x.css('a')[1].attr('href')
 			l[:title] = x.css('a')[1].text()
@@ -14,6 +14,7 @@ class ListingsController < ApplicationController
 			hood = Neighborhood.find_by_name( l[:c_hood].to_s )
 			l[:info] = x.css('.pnr').text
 			l[:crime_percentage] = hood.crime_percentage() unless hood.nil? 
+			
 
 			case l[:crime_percentage]
 				when (20...100)
